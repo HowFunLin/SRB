d8e3b8da5f8787f0cb67c5060074bc6e7f1ca958
HowFunLin <how-fun@foxmail.com>
2021-12-19 21:06:29 +0800

    创建基本项目

---
+++ service-core/src/main/java/com/atguigu/srb/core/controller/TransFlowController.java
@@ -0,0 +1,21 @@
+package com.atguigu.srb.core.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 交易流水表 前端控制器
+ * </p>
+ *
+ * @author Riyad
+ * @since 2021-12-19
+ */
+@RestController
+@RequestMapping("/transFlow")
+public class TransFlowController {
+
+}
+
